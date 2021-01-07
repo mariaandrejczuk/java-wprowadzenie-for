8798a28532462ce2708f65437e54b424586ad622
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-07 14:33:42 +0100

    #9 częściowe rozwiązanie zadania 4

--- src/SortArray.java
+++ src/SortArray.java
@@ -1,7 +1,7 @@
 public class SortArray {
     public static void main(String[] args) {
 
-        int[] numbers = {5,2,-5,1,5,22,5};
+        int[] numbers = {5, 2, -5, 1, 5, 22, 5};
         /*
         1-szy obieg - dwa porówania
         5,2,-5
@@ -12,9 +12,17 @@ public class SortArray {
          */
 
         //wyświetlenie elementów:
-        for(int i = 0; i < numbers.length; i++){
-            System.out.println(numbers[i]+",");
-        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i] + ",");
 
+            //wyświetlenie elementów:
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    int tmp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = tmp;
+                }
+            }
+        }
     }
 }
