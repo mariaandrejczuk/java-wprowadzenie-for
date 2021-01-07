76ea9e9cc2d687af713a8eaa84d1bb4750b0a29e
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-07 13:46:10 +0100

    #3 rozwiązanie zadania 2 - wyświetlenie liczb parzystych z zakresu 62349 - 62392

--- src/DisplayNumbers.java
+++ src/DisplayNumbers.java
@@ -1,11 +1,25 @@
+
+//Zadanie 1
+
 public class DisplayNumbers {
     public static void main(String[] args) {
-        System.out.println(91);
-        System.out.println(90);
-        System.out.println(89);
 
-        for(int i = 91; i>= -7;i--){
+        System.out.println("---------- ZADANIE 1 ----------");
+//        System.out.println(91);
+//        System.out.println(90);
+//        System.out.println(89);
+
+        for (int i = 91; i >= -7; i--) {
             System.out.println(i);
+        }
+
+        System.out.println("---------- ZADANIE 2 ----------");
+
+        for (int i = 62349; i <= 62392; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
+
