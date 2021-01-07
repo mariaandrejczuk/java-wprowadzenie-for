3df837419e9d5d9aadd4eb9c5e8e8fd569e425c5
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-07 13:33:17 +0100

    #1 utworzenie pliku DisplayNumbers.java

---
+++ src/DisplayNumbers.java
@@ -0,0 +1,8 @@
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        System.out.println(91);
+        System.out.println(90);
+        System.out.println(89);
+        
+    }
+}
